b07a50458f9e0310cc9fb4135798a350b7a4d28b
kartikchoudhary7199 <kartikchoudhary7199@gmail.com>
2026-03-13 00:31:41 +0530

    fix project structure

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type:String,
+        unique:[true, "username already exist" ],
+        required:[true, "username is required" ]
+    },
+    email: {
+        type:String,
+        unique:[true, "email already exist" ],
+        required:[true, "email is required" ] 
+    },
+    password: {
+        type:String,
+         required:[true, "password is required" ] 
+    },
+    Bio: String,
+    profileImage: {
+        type:String,
+        default:"https://ik.imagekit.io/xklo2kwyw/OIP.3IsXMskZyheEWqtE3Dr7JwHaGe_w=207&h=181&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"
+    }
+})
+
+
+const userModel = mongoose.model("user", userSchema)
+
+module.exports = userModel
---
+++ backend/src/routes/post.routes.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const postRouter = express.Router()
+const postController = require("../controllers/post.controller")
+const multer = require("multer")
+const upload = multer({ storage: multer.memoryStorage() })
+
+
+
+
+postRouter.post("/", upload.single("image"), postController.createPostController)
+
+
+
+postRouter.get("/", postController.getPostController)
+
+
+
+postRouter.get("/details/:postId", postController.getPostDetailsController)
+
+
+module.exports = postRouter
