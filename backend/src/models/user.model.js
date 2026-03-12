837fca331b90af301c441d5e023ec619d90401c2
kartikchoudhary7199 <kartikchoudhary7199@gmail.com>
2026-03-13 02:29:46 +0530

    added follow model and today's progress

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -19,7 +19,15 @@ const userSchema = new mongoose.Schema({
     profileImage: {
         type:String,
         default:"https://ik.imagekit.io/xklo2kwyw/OIP.3IsXMskZyheEWqtE3Dr7JwHaGe_w=207&h=181&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"
-    }
+    },
+    followers:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users"
+    }],
+    following:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users"
+    }]
 })
 
 
